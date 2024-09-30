625f626fe6d3365f414be95ecc836a5144654340
s1de <79643224+s0uths1de@users.noreply.github.com>
2024-09-30 16:19:51 +0800

    a0.1

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div style="position: relative; width: 100%; height: 500px;">
+    <Typewriter class="typewrite" @click="clickCopy" v-if="initialText" :text="initialText"
+                style=" display:block;position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 100"/>
+        <img style="width: 100%; height: 100%;" :src="imgUrl" alt="Scenic Image"/>
+    <div class="introduce">
+      <span
+          style=" display: block; width: 280px;color:white;font-size: 18px;">
+      涟 漪 - 一 个 没什 么 用 的 网 页
+      </span>
+    </div>
+  </div>
+
+  <div class="footer">
+    <a href="https://github.com/s0uths1de">
+      S1de个人仓库
+    </a>
+    <br>
+    <br>
+    <a href="https://beian.miit.gov.cn/">
+      赣ICP备2024033384号
+    </a>
+  </div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue';
+import Typewriter from './components/Typewriter.vue';
+import axios from 'axios';
+
+const imgUrl = 'https://api.dujin.org/pic/fengjing';
+const sentenceUrl = 'https://tenapi.cn/v2/yiyan';
+
+const sentence = ref('');
+const initialText = ref('');
+
+onMounted(() => {
+  axios({
+    method: 'get',
+    url: sentenceUrl,
+    responseType: 'json'
+  }).then((response) => {
+    sentence.value = response.data;
+    initialText.value = sentence.value;
+  }).catch((error) => {
+    console.error('Error fetching sentence:', error);
+  });
+  let banner = "%c _ \\_)         |          \n" +
+      "%c   / | _ \\ _ \\ |  -_)(_-< \n" +
+      "%c_|_\\_|.__/.__/_|\\___|___/ \n" +
+      "%c     _|  _|               \n";
+  let styles = [
+    "color: #008000; font-weight: bold;",
+    "color: #000080; font-weight: bold;",
+    "color: #FF0000; font-weight: bold;",
+    "color: #FFA500; font-weight: bold;"
+  ];
+  console.log.apply(console, [banner].concat(styles));
+});
+
+async function clickCopy() {
+  try {
+    const blob = new Blob([sentence.value], {type: 'text/plain'});
+    const clipboardItem = new ClipboardItem({'text/plain': blob});
+    await navigator.clipboard.write([clipboardItem]);
+    alert('复制完成')
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+</script>
+
+<style>
+@import '@/assets/css/default.css';
+
+.footer {
+  width: 65%;
+  height: 3em;
+  line-height: 1em;
+  color: white;
+  margin: 20px auto;
+  text-align: center;
+}
+
+.introduce {
+  position: absolute;
+  top: 7%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 50px;
+  text-align: center
+}
+
+.introduce :hover {
+  background: rgba(255, 255, 255, .5);
+  backdrop-filter: blur(3px);
+  border-radius: 15px;
+}
+
+</style>
---
+++ src/components/Typewriter.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="text" class="centered-text">
+    <span class="textInner">{{ textInner }}</span>
+    <span class="typed-cursor typed-cursor--blink" aria-hidden="true">&nbsp;_</span>
+  </div>
+</template>
+
+<script setup>
+import {ref, onMounted, onBeforeUnmount} from 'vue';
+
+// 定义 props
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  }
+});
+
+const textInner = ref('');
+let intervalId = null; // 用于存储定时器 ID
+
+const typewriterEffect = (text) => {
+  let charIndex = 0;
+  const delayPerChar = text.length > 20 ? 100 : 200;
+
+  intervalId = setInterval(() => {
+    if (charIndex < text.length) {
+      textInner.value += text[charIndex];
+      charIndex++;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, delayPerChar);
+};
+
+onMounted(() => {
+  setTimeout(() => {
+    typewriterEffect(props.text);
+  }, 200);
+});
+
+// 在组件卸载之前清除定时器
+onBeforeUnmount(() => {
+  clearInterval(intervalId);
+});
+
+</script>
+
+<style scoped>
+body {
+  background-color: black;
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.typed-cursor, .textInner {
+  display: inline-block;
+}
+
+.typed-cursor--blink {
+  animation: blink 0.7s infinite;
+}
+
+@keyframes blink {
+  50% {
+    opacity: 0;
+  }
+}
+
+.centered-text {
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  text-shadow: 1px 1px 2px black;
+  white-space: nowrap;
+  text-align: center;
+}
+
+.centered-text:hover {
+  background: rgba(255, 255, 255, .5);
+  backdrop-filter: blur(3px);
+  border-radius: 15px;
+  height: 40px;
+  line-height: 40px;
+}
+
+#text{
+  padding: 0 18px 0 28px;
+}
+
+</style>
